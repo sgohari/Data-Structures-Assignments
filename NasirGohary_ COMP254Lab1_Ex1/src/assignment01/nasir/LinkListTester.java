4029efbc1e6b9be5f9b33578bd16630bf673b642
sgohary <nasir_2005@msn.com>
2019-01-30 13:49:44 -0500

    Folder changs

--- NasirGohary_ COMP254Lab1_Ex1/src/assignment01/nasir/LinkListTester.java
+++ NasirGohary_ COMP254Lab1_Ex1/src/assignment01/nasir/LinkListTester.java
@@ -4,32 +4,45 @@ public class LinkListTester {
 
 	public static void main(String[] args) {
 
-		System.out.println("\nPrinting Single Linked List with no NODE is added!!\n");
+		System.out.println("\n<=====Singly Linked List Demo======>");
+		System.out.println("Printing Empty Linked-List");
 		SingleLinkList<String> singleLS = new SingleLinkList<String>();
 		singleLS.showSinglyLinkedList();
-		System.out.println("\nAdding Four NODES and Displaying All\n ");
+		System.out.println("\n<=====++++++++++++++++++++++++=====>");
+		System.out.println("Adding Four NODES and Displaying All ");
 		singleLS.addFirst("Node01");
 		singleLS.addFirst("Node02");
 		singleLS.addFirst("Node03");
 		singleLS.addFirst("Node04");
 		singleLS.showSinglyLinkedList();
-		System.out.println("\nRemoving 'Node02' from the list\n ");
+		System.out.println("<=====++++++++++++++++++++++++=====>\n");
+		System.out.println("\n************************************");
+		System.out.println("Removing 'Node02' from the list ");
 		singleLS.removeAt(3);
 		singleLS.showSinglyLinkedList();
-		System.out.println("The item 3 was deleted..\n");
-		System.out.println();
+		System.out.println("Item 3 was deleted..");
+		System.out.println("************************************\n");
+		System.out.println("************************************");
+		System.out.println("Deleting 'Node03' from the list ");
 		singleLS.removeAt(2);
-		System.out.println("The item 2 was deleted.");
+		System.out.println("\nItem 2 was deleted.");
 		singleLS.showSinglyLinkedList();
-		System.out.println();
+		System.out.println("************************************\n");
+		System.out.println("************************************");
+		System.out.println("Deleting 'Node01' from the list ");
 		singleLS.removeAt(2);
-		System.out.println("Another item 2 was deleted.");
+		System.out.println("Item 1 was deleted.");
 		singleLS.showSinglyLinkedList();
-		System.out.println();
+		System.out.println("************************************\n");
+		System.out.println("************************************");
+		System.out.println("Deleting 'Node04' from the list ");
 		singleLS.removeAt(1);
 		singleLS.showSinglyLinkedList();
-
+		System.out.println("************************************");
+		System.out.println("************************************");
+		System.out.println("Displaying the size of Linked List");
 		System.out.println(singleLS.size());
+		System.out.println("************************************");
 		System.out.println();
 		singleLS.insertAt("newNode1", 1);
 		singleLS.insertAt("newNode5", 2);
